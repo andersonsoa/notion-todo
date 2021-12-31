af627c1c008a7ecbe473d38b4be35419b3a8af83
Anderson Soares <anderson.ceu@gmail.com>
2021-12-30 23:10:20 -0400

    configuracao do notion api, eg: database

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { notion } from "../../lib/notion";
 
-type Data = {
-  name: string
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+    });
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json(data);
+  } catch {
+    res.status(400).json({ error: "um erro aconteceu" });
+  }
 }
